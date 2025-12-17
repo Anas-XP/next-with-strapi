7e2963b8202e5c3ecd036cde0dccc2836ab71a1b
Anas Okasha <anas@bold-xp.com>
2025-12-17 04:25:14 +0200

    feat: add environment configuration, implement dashboard and login components, enhance loading state, and create not found page

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,25 +1,23 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Card, CardFooter } from "@/components/ui/card";
-import { useLogout } from "@/features/auth/hooks/use-logout.hook";
-import { LogOutIcon } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { DashboardMainCard } from "@/features/dashboard/components/dashboard-main-card";
+import { HomeIcon } from "lucide-react";
+import Link from "next/link";
 
 const DashboardPage = () => {
-  const { mutate: logout, isPending } = useLogout();
   return (
-    <Card className="container mx-auto mt-3">
-      <CardFooter>
-        <Button
-          variant={"destructive"}
-          onClick={() => {
-            logout();
-          }}
-          disabled={isPending}
-        >
-          <LogOutIcon /> Logout
-        </Button>
-      </CardFooter>
-    </Card>
+    <div className="flex min-h-dvh flex-col items-center justify-center">
+      <Link
+        href={"/"}
+        className={buttonVariants({
+          variant: "default",
+          size: "icon-lg",
+          className: "mx-auto",
+        })}
+      >
+        <HomeIcon />
+      </Link>
+      <DashboardMainCard />
+    </div>
   );
 };
 
--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,11 +1,13 @@
-import { Logo } from "@/components/logo";
 import { buttonVariants } from "@/components/ui/button";
 import { LoginFormCard } from "@/features/auth/components/login-form-card";
+import { sleep } from "@/lib/helpers.utils";
+import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  await sleep(1);
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link
           href={"/"}
@@ -15,7 +17,7 @@ const LoginPage = () => {
             className: "mx-auto",
           })}
         >
-          <Logo />
+          <HomeIcon />
         </Link>
 
         <LoginFormCard />
